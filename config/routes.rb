aa5b3992d41aa83851d316ce5bf04948537c7209
Daniel Lobato <elobatocs@gmail.com>
2015-02-02 09:06:35 +0100

    Fixes #7874 - Docker Contaieners API

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
     namespace :api, :defaults => { :format => 'json' } do
       scope "(:apiv)", :module => :v2, :defaults => { :apiv => 'v2'}, :apiv => /v2/,
         :constraints => ApiConstraints.new(:version => 2) do
-        resources :containers, :controller => 'foreman_docker/api/v2/containers', :only => [:index, :create, :show, :destroy] do
+        resources :containers, :container => 'foreman_docker/api/v2/containers', :only => [:index, :create, :show, :destroy] do
           member do
             get :log
             put :power
